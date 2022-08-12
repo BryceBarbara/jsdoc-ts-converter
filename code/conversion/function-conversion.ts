28064d0c0dedb475e21ffb2b764ba3f3548f19cc
Bryce Barbara <BryceBarbara@users.noreply.github.com>
2022-08-12 11:55:32 -0700

    Add some polish
    * Remove unused functions, components, endpoints
    * Update github link URL in the footer

--- code/conversion/function-conversion.ts
+++ code/conversion/function-conversion.ts
@@ -4,9 +4,9 @@ import type { ConversionContext } from './general-types'
 import { JSDocConverter } from './jsdoc-conversion'
 
 interface FunctionParamInfo {
-  name: string;
-  type?: string;
-  optional: boolean;
+  name: string
+  type?: string
+  optional: boolean
 }
 
 export class FunctionConverter {
@@ -17,92 +17,98 @@ export class FunctionConverter {
   ): string | undefined {
     const jsDoc = node
       .getChildren(context.sourceFile)
-      .find((e) => ts.isJSDoc(e)) as ts.JSDoc | undefined;
-    if (!jsDoc) return;
+      .find(e => ts.isJSDoc(e)) as ts.JSDoc | undefined
+    if (!jsDoc)
+      return
 
     const description = JSDocConverter.getJsDocCommentText(
       jsDoc.comment,
       context,
-    );
+    )
 
     // generate the function jsdoc comment
-    let methodCommentText = `${description ?? 'TODO: add description'}`;
-    const params: FunctionParamInfo[] = [];
-    let returnType: string | undefined;
-    let typeArgumentsText: string = '';
+    let methodCommentText = `${description ?? 'TODO: add description'}`
+    const params: FunctionParamInfo[] = []
+    let returnType: string | undefined
+    let typeArgumentsText = ''
     if (jsDoc.tags) {
       for (const tag of jsDoc.tags) {
         if (ts.isJSDocParameterTag(tag)) {
           const param: FunctionParamInfo = {
             name: tag.name?.getText(context.sourceFile) ?? 'unnamedParam',
             optional: tag.isBracketed,
-          };
+          }
           if (!tag.typeExpression?.type) {
             // no type
-          } else if (ts.isJSDocTypeLiteral(tag.typeExpression.type)) {
+          }
+          else if (ts.isJSDocTypeLiteral(tag.typeExpression.type)) {
             // type expression like { foo: string }
             // param.type = tag.typeExpression.type.getText(context.sourceFile);
-            param.type = 'UNSUPPORTED-COMPLEX-TYPE';
-          } else if (ts.isJSDocTypeExpression(tag.typeExpression.type)) {
-            param.type = 'EXPRESSION';
-          } else {
+            param.type = 'UNSUPPORTED-COMPLEX-TYPE'
+          }
+          else if (ts.isJSDocTypeExpression(tag.typeExpression.type)) {
+            param.type = 'EXPRESSION'
+          }
+          else {
             // something else
-            param.type = tag.typeExpression.type.getText(context.sourceFile);
+            param.type = tag.typeExpression.type.getText(context.sourceFile)
           }
           let paramComment = JSDocConverter.getJsDocCommentText(
             tag.comment,
             context,
-          );
-          methodCommentText += `\n@param ${param.name}`;
+          )
+          methodCommentText += `\n@param ${param.name}`
           if (paramComment) {
-            paramComment = paramComment.trim();
+            paramComment = paramComment.trim()
 
             // if the comment starts with ' - ' then remove it
-            if (paramComment.startsWith('-')) {
-              paramComment = paramComment.substring(1).trim();
-            }
-            methodCommentText += ` ${paramComment}`;
+            if (paramComment.startsWith('-'))
+              paramComment = paramComment.substring(1).trim()
+
+            methodCommentText += ` ${paramComment}`
           }
-          params.push(param);
-        } else if (ts.isJSDocReturnTag(tag)) {
-          returnType =
-            tag.typeExpression?.type?.getText(context.sourceFile) ?? 'unknown';
-        } else if (ts.isJSDocTemplateTag(tag)) {
+          params.push(param)
+        }
+        else if (ts.isJSDocReturnTag(tag)) {
+          returnType
+            = tag.typeExpression?.type?.getText(context.sourceFile) ?? 'unknown'
+        }
+        else if (ts.isJSDocTemplateTag(tag)) {
           typeArgumentsText = (tag.typeParameters ?? [])
-            .map((e) => e.getText(context.sourceFile))
-            .join(', ');
-          typeArgumentsText = '<' + typeArgumentsText + '>';
+            .map(e => e.getText(context.sourceFile))
+            .join(', ')
+          typeArgumentsText = `<${typeArgumentsText}>`
         }
       }
     }
 
     // generate the function signature
-    let methodSignature = `${name}${typeArgumentsText}(`;
+    let methodSignature = `${name}${typeArgumentsText}(`
     for (let i = 0; i < params.length; i++) {
-      const param = params[i];
-      methodSignature += `${param.name}`;
-      if (param.optional) {
-        methodSignature += '?';
-      }
-      if (param.type) {
-        methodSignature += `: ${param.type}`;
-      } else {
-        methodSignature += `: unknown`;
-      }
-      if (i < params.length - 1) {
-        methodSignature += ', ';
-      }
+      const param = params[i]
+      methodSignature += `${param.name}`
+      if (param.optional)
+        methodSignature += '?'
+
+      if (param.type)
+        methodSignature += `: ${param.type}`
+
+      else
+        methodSignature += ': unknown'
+
+      if (i < params.length - 1)
+        methodSignature += ', '
     }
-    if (name !== "constructor")
-      methodSignature += `): ${returnType || 'unknown'};`;
+    if (name !== 'constructor')
+      methodSignature += `): ${returnType || 'unknown'};`
     else
-      methodSignature += ');';
+      methodSignature += ');'
 
     const functionText = `${Formatting.multiLineComment(
       methodCommentText,
       context.options,
-    )}\n${methodSignature}`;
+    )}\n${methodSignature}`
 
-    return functionText;
+    return functionText
   }
 }
